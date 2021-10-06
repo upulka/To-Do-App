53e662c80dc135f6528c7e2595250e8cbf52af09
upulka <67848552+upulka@users.noreply.github.com>
2021-10-06 12:50:01 +0530

    Commit Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+      <Router>
+        <Switch>
+            <Route path = "/" >
+                <Home/>
+            </Route>
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
